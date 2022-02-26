32218edb68bd1019c8da47b3d1c3999fd4c03671
ozzyZig1994 <adio_him@hotmail.com>
2022-02-26 14:01:15 -0600

    Tomando foto y apagar la camara

--- public/js/app.js
+++ public/js/app.js
@@ -517,6 +517,11 @@ btnTomarFoto.on('click', () => {
 
     console.log('Botón tomar foto');
     
+    foto = camara.tomarFoto();
+
+    camara.apagar();
+
+    console.log(foto);
 });
 
 
--- public/js/camara-class.js
+++ public/js/camara-class.js
@@ -18,6 +18,32 @@ class Camara {
     }
 
     apagar() {
+        this.videoNode.pause();
 
+        if (this.stream) 
+            this.stream.getTracks()[0].stop();   
+    }
+
+    tomarFoto () {
+        // Crear elemento canvas para ahi renderizar la foto
+        let canvas = document.createElement('canvas');
+
+        // Colocar dimensiones igual al elemento del video
+        canvas.setAttribute('width', 300);
+        canvas.setAttribute('height', 300);
+
+        // Obtener el contexto del canvas
+        let context = canvas.getContext('2d');
+
+        // Imagen dentro del canvas
+        context.drawImage(this.videoNode, 0, 0, canvas.width, canvas.height);
+
+        this.foto = context.canvas.toDataURL();
+
+        // limpieza
+        canvas = null;
+        context = null;
+
+        return this.foto;
     }
 }
